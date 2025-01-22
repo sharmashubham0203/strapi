ecc41bd8838cc2a49eecd0742f5fac3c9f811484
Shubham Sharma <138666350+sharmashubham0203@users.noreply.github.com>
2025-01-23 01:22:06 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -64,17 +64,15 @@
 // };
 
 module.exports = ({ env }) => ({
-  
-  connection : {
-      client = 'postgres' ,
-      connection: {
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        user: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', true) 
-      },
+  connection: {
+    client: 'postgres', // Use ":" instead of "=" for key-value pairs
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
-  });
-
+  },
+});
